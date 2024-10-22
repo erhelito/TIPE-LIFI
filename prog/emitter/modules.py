52032892fe48e1811612515be32c87ac2cbcd2c1
erhelito <erhelito@yahoo.fr>
2024-10-22 18:55:07 +0200

    creating docstrings

--- prog/emitter/modules.py
+++ prog/emitter/modules.py
@@ -2,19 +2,19 @@ from time import sleep
 from machine import Pin, PWM # type: ignore
 from math import cos, pi
 
-def power(high=True) -> None:
+def power(high:bool=True) -> None:
     """Simply turn on/off the builtin led"""
     led = Pin('LED', Pin.OUT)
     if high: led.high()
     else: led.low()
 
-def laser_on(high=True) -> None:
+def laser_on(high:bool=True) -> None:
     """Simply turn on/off the laser"""
     laser = Pin(16, Pin.OUT)
     if high: laser.high()
     else: laser.low()
 
-def visualize(value, min=0, max=1) -> None:
+def visualize(value:int, min:int=0, max:int=1) -> None:
     """Visualize a value between a min/max value, thanks to the builtin led.
     Default: min=0, max=1"""
 
@@ -25,7 +25,8 @@ def visualize(value, min=0, max=1) -> None:
     value = int(value*65535) # convert to PWM
     led.duty_u16(value)
 
-def freq(f) :
+def freq(f:int) -> None:
+    """Tilt the emitter at a given frequency, for 3 periods"""
     for i in range(3*f):
         led = Pin(16, Pin.OUT)
         led.value(1)
@@ -33,26 +34,28 @@ def freq(f) :
         led.value(0)
         sleep(1/(2*f))
 
-def test_pwm(value):
+def test_pwm(value:int) -> None:
+    """Test the PWM module for the laser"""
     #50000 - 65535
     led = PWM(Pin(16, Pin.OUT))
     led.freq(1000)
 
     led.duty_u16(value)
 
-def sine():    
+def sine() -> None:
+    """Sine wave on the laser"""
     led = PWM(Pin(16, Pin.OUT))
     led.freq(1000)
 
     list = [cos(x*pi/180) for x in range(0,360)]
     list = [int(x*(65535-50000)+50000) for x in list]
-    print(list)
 
     for i in list:
         led.duty_u16(i)
         sleep(0.01)
 
-def ramp():
+def ramp() -> None:
+    """Ramp on the laser"""
     led = PWM(Pin(16, Pin.OUT))
     led.freq(1000)
 
